5e47a6ede557ecd730d5e0e0811fbbb1243d95c7
Haokang Den <haokang.den@gmail.com>
2021-12-11 23:13:08 +0900

    release 0.3.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.hden/aws-xray-sdk-clj "0.2.0-SNAPSHOT"
+(defproject com.github.hden/aws-xray-sdk-clj "0.3.0-SNAPSHOT"
   :description "A light wrapper for aws-xray-sdk-java"
   :url "https://github.com/hden/aws-xray-sdk-clj"
   :license {:name "Apache License, Version 2.0"
